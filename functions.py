ecc400ad9f313d241e595e24eb3a7c227702d7c5
trishakumar <58877381+trishakumar@users.noreply.github.com>
2022-09-22 11:38:41 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,30 @@
+x = 1
+y = 2
+x + y
+
+def add_numbers(x,y):
+    return x + y
+
+add_numbers(1,2)
+
+def add_numbers(x, y, z=None):
+    if(z == None):
+        return x + y
+    else:
+        return x + y + z
+
+print(add_numbers(1,2))
+
+print(add_numbers(1,2,3))
+        
+def add_numbers(x, y, z=None, flag=False):
+    if(flag):
+        print("flag is true")
+    if(z==None):
+        return x+y
+    else:
+        return x+y+z
+    
+print(add_numbers(1,2,flag=True))
+        
+        
